d173767b6d4a22f23a69169b49c5ae4e4a05617d
guoliang <guoliang@enovatemotors.com>
2020-03-12 22:39:42 +0800

    guoliang1

--- application/models/user_model.py
+++ application/models/user_model.py
@@ -5,5 +5,10 @@ class UserModel(db.Model):
     __tablename__ = 'user'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), nullable=True)
-    email = db.Column(db.String(50), nullable=True)
+    name = db.Column(db.String(20), unique=True, nullable=True)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(50), unique=True, nullable=False)
+    usergroup = db.Column(db.String(2), unique=True, nullable=True)
+    membership = db.Column(db.String(2), unique=True, nullable=True)
+    membership_starttime = db.Column(db.DateTime, unique=True, nullable=True)
+    membership_endtime = db.Column(db.DateTime, unique=True, nullable=True)
