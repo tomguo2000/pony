09eb14ccf948e27680e990c08b9b0b4a2d1ab187
guoliang <guoliang@enovatemotors.com>
2020-03-13 23:12:55 +0800

    update view

--- application/views/user_view.py
+++ application/views/user_view.py
@@ -15,7 +15,12 @@ class GetUserView(BaseView):
         return {
             'user_id': user_info.get('id'),
             'user_name': user_info.get('name'),
-            'user_email': user_info.get('email')
+            'user_email': user_info.get('email'),
+            'user_password': user_info.get('password11'),
+            'user_usergroup': user_info.get('usergroup'),
+            'user_membership': user_info.get('membership'),
+            'user_membership_starttime': user_info.get('membership_starttime'),
+            'user_membership_endtime': user_info.get('membership_endtime')
         }
 
     def other_function(self):
