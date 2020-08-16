f1413e34969b3610743fb2e6fe13791502d186c9
guoliang <guoliang@enovatemotors.com>
2020-08-16 11:12:57 +0800

    add status into setting_model

--- application/models/setting_model.py
+++ application/models/setting_model.py
@@ -6,7 +6,8 @@ class SettingModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(50))
-    value = db.Column(db.String(200))
+    value = db.Column(db.String(255))
+    status = db.Column(db.String(255))
     timestamp = db.Column(db.BigInteger)
 
     def __repr__(self):
