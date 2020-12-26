35e47f146990b17586a034087036b3f7d3e8cc60
guoliang <guoliang@enovatemotors.com>
2020-12-26 11:27:17 +0800

    add swagger into start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 ps -efww| grep app:app | grep -v grep | cut -c 9-15|xargs kill -9
 cd application
 /usr/bin/python3 /usr/local/bin/gunicorn -b 0.0.0.0:8080 app:app &
+docker run -p 81:8080 -e SWAGGER_JSON=/api.yaml -v /root/pony/openapi/api.yaml:/api.yaml swaggerapi/swagger-ui &
